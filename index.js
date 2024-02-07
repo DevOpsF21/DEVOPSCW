f5100b167e4ccbb0795e0d7e2d1a2b6f6e40792d
mrkhmath <kh_math@ymail.com>
2024-02-07 20:48:39 +0400

    installing packages

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-"Hi Group 1, we are live"
+const express = require("express");
+const app = express();
+
+users = [
+  {
+    username: "user1",
+    password: "password1",
+  },
+  {
+    username: "user2",
+    password: "password2",
+  },
+  {
+    username: "user3",
+    password: "password3",
+  },
+];
+app.get("/users", (req, resp) => {
+  resp.json(users);
+});
+app.listen(3000);
