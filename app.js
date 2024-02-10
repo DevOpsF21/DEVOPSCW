055407d08f6b095136e8517dc6288f1b9df5e844
IliaSvetlichnyi <svetlis2000@gmail.com>
2024-02-10 12:42:54 +0400

    Tried to connect to a specific collection

--- app.js
+++ app.js
@@ -8,12 +8,24 @@ const port = 3000;
 app.use(express.json());
 
 const password = '10vVkcArvkRhFqGe';
-const mongoDB = `mongodb+srv://svetlis2000:${password}@initialpatientregistrat.kw4yj15.mongodb.net/?retryWrites=true&w=majority`;
+const dbName = 'DevOps'; // Name of the database
 
-mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+const mongoDB = `mongodb+srv://svetlis2000:${password}@initialpatientregistrat.kw4yj15.mongodb.net/${dbName}`;
+
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+};
+
+mongoose.connect(mongoDB, options);
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', function () {
+    console.log(`Connected to the ${dbName} database`);
+});
 
 app.get('/', (req, res) => {
     res.send('Hello World! Connected to MongoDB.');
@@ -44,3 +56,51 @@ app.post('/register-patient', async (req, res) => {
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`);
 });
+
+// Previous version of app.js
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const Patient = require('./models/patient');
+
+// const app = express();
+// const port = 3000;
+
+// app.use(express.json());
+
+// const password = '10vVkcArvkRhFqGe';
+// const mongoDB = `mongodb+srv://svetlis2000:${password}@initialpatientregistrat.kw4yj15.mongodb.net/?retryWrites=true&w=majority`;
+
+// mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// const db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// app.get('/', (req, res) => {
+//     res.send('Hello World! Connected to MongoDB.');
+// });
+
+// app.post('/register-patient', async (req, res) => {
+//     try {
+//         // Directly passing req.body to the Patient constructor
+//         const patient = new Patient(req.body);
+//         await patient.save();
+//         res.status(201).send(patient);
+//     } catch (error) {
+//         console.error(error);
+//         // Check if the error is a Mongoose ValidationError
+//         if (error.name === 'ValidationError') {
+//             // Map the errors to extract messages
+//             const messages = Object.values(error.errors).map(val => val.message);
+//             // Send back a 400 status with the validation error messages
+//             res.status(400).send({ errors: messages });
+//         } else {
+//             // For any other type of error, send a 500 status
+//             res.status(500).send('An unexpected error occurred');
+//         }
+//     }
+// });
+
+
+// app.listen(port, () => {
+//     console.log(`Server listening at http://localhost:${port}`);
+// });
--- models/patient.js
+++ models/patient.js
@@ -53,7 +53,69 @@ const patientSchema = new mongoose.Schema({
         required: false
     }]
 });
+const collectionName = 'PatientRegistration'; // Name of the collection
 
-const Patient = mongoose.model('Patient', patientSchema);
-
+const Patient = mongoose.model('Patient', patientSchema, collectionName);
 module.exports = Patient;
+
+
+// Previous version of patient.js
+// const mongoose = require('mongoose');
+// const validator = require('validator'); // npm install validator
+
+// const patientSchema = new mongoose.Schema({
+//     name: {
+//         type: String,
+//         required: [true, 'Name is required'],
+//         trim: true
+//     },
+//     dateOfBirth: {
+//         type: Date,
+//         required: [true, 'Date of birth is required'],
+//         // Custom validator to check if the date of birth is realistic
+//         validate: {
+//             validator: function (value) {
+//                 return value < new Date();
+//             },
+//             message: 'Date of birth must be in the past'
+//         }
+//     },
+//     role: {
+//         type: String,
+//         required: [true, 'Role is required'],
+//         enum: {
+//             values: ['patient', 'doctor', 'nurse', 'admin'],
+//             message: 'Role `{VALUE}` is not supported'
+//         }
+//     },
+//     contactDetails: {
+//         phone: {
+//             type: String,
+//             required: [false, 'Phone number is optional'],
+//             validate: {
+//                 validator: function (value) {
+//                     return validator.isMobilePhone(value);
+//                 },
+//                 message: 'Invalid phone number'
+//             }
+//         },
+//         email: {
+//             type: String,
+//             required: [false, 'Email is optional'],
+//             validate: {
+//                 validator: function (value) {
+//                     return validator.isEmail(value);
+//                 },
+//                 message: 'Invalid email'
+//             }
+//         }
+//     },
+//     knownDiseases: [{
+//         type: String,
+//         required: false
+//     }]
+// });
+
+// const Patient = mongoose.model('Patient', patientSchema);
+
+// module.exports = Patient;
